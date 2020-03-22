c8bc356830a6b7055e1e90e746a94125a893e069
DeAndre Boston <dboston3@yahoo.com>
2020-03-21 22:23:11 -0500

    Update doubly-linked-list.js

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,1 +1,15 @@
 
+class Node {
+    constructor(){
+        this.value = value;
+    d    this.prev= null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+}
