8624515382d3646b5c8cdf9b921d0eb3b1629eaf
DeAndre Boston <dboston3@yahoo.com>
2020-04-01 22:24:47 -0500

    Update doubly-linked-list.js

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,4 +1,4 @@
-
+//create a class for the node. The node is what holds the value, and points to the next node if applicable.
 class Node {
     constructor(){
         this.value = value;
@@ -7,6 +7,9 @@ class Node {
     }
 }
 
+//This is the actual structure for the linked list. Technically this structure consist of nothing but head and a tail.
+//The rest of the nodes are sort nested within the first node/head, and the last node is considered the tail. 
+//You will also create all of your methods here.
 class DoublyLinkedList {
     constructor(){
         this.head = null;
